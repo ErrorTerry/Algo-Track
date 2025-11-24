2e3de19f9cef4e22d969f6496399a82aa69268b0
SHU <jisu.kimjss@gmail.com>
2025-11-24 16:20:37 +0900

    feat(frontend): 로그인 성공 페이지 구현 #60
    - 카카오 로그인 성공 후 이동하는 환영 페이지 추가
    - IDE + 알고리즘 사전 + 목표 설정 + 학습 통계 기능 소개 카드 구성
    - 백준 이동 버튼 및 사용자 닉네임 표시 기능 구현
    - 로그인 관련 부분 Pretendard 폰트 적용
    - 개발 편의를 위한 임시 로그인 버튼 추가 (localStorage 저장)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./login/LoginPage";
+import LoginSuccess from "./login/LoginSuccess.tsx";
 
 export default function App() {
-    return <>
-        <LoginPage />
-    </>
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* 로그인 페이지 */}
+                <Route path="/login" element={<LoginPage />} />
+
+                {/* 로그인 후 온보딩 페이지 */}
+                <Route path="/login-success" element={<LoginSuccess />} />
+
+                {/* 기본 URL 접근 시 자동으로 /login으로 이동 */}
+                <Route path="*" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
