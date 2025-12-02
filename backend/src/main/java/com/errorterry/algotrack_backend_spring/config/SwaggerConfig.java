29f209d26f909706fe3b70726355daca9ae7dc87
SnowCloudWay <snowcloudway@gmail.com>
2025-12-02 14:22:11 +0900

    feat(backend): [Backend] Swagger Authorization(Bearer Token) 입력 기능 추가 #106
    - Swagger에 Bearer Token 인증 스키마 추가
    - Swagger UI 상단에 Authorize 버튼 활성화
    - Authorization 헤더 자동 적용 기능 설정
    - 기존 서버 URL 설정과 함께 SecurityScheme, SecurityRequirement 구성 추가

--- backend/src/main/java/com/errorterry/algotrack_backend_spring/config/SwaggerConfig.java
+++ backend/src/main/java/com/errorterry/algotrack_backend_spring/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.errorterry.algotrack_backend_spring.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +15,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        String schemeName = "BearerAuth";
+
         return new OpenAPI()
+                // SWAGGER 인증 버튼 생성 부분
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName,
+                                new SecurityScheme()
+                                        .name(schemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 // 현재 도메인(https://algotrack.store)을 기준으로 /api 같은 식으로 날림
                 // 완전 고정하고 싶으면: new Server().url("https://algotrack.store")
                 .servers(List.of(new Server().url("/")));
